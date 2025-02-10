11756f7b81d45082dd9c78bcbb9869bada98e5c9
Kinji <linhqwertyuiop2gmail.com>
2025-02-10 22:19:26 +0700

    update 10_2_2025

--- src/app/tai-khoan/dang-ky/page.tsx
+++ src/app/tai-khoan/dang-ky/page.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import "./register.css";
+import "../layout.css";
 
 const RegisterPage = () => {
+  const [phoneMode, setPhoneMode] = useState<boolean>(false);
   return (
     <div className="register">
       <div className="grid wide">
@@ -15,28 +18,52 @@ const RegisterPage = () => {
               alt="Picture of the author"
             />
           </div>
-          <div className="c-4">
-            <form>
+          <div className="c-4 account__container">
+            <form className="account__form">
               <div className="register__head">Đăng ký</div>
-              <input type="email" />
-              <input type="number" />
-              <div className="register__clause">
-                <input type="checkbox" name="" id="" />
-                <span>Tôi đồng ý với điều khoản & điều kiện của ...</span>
-              </div>
-              <button>Đăng ký</button>
-            </form>
-            <div></div>
-            <div>Hoặc</div>
-            <div></div>
 
-            <div className="register__change">
-              <i className="fa-solid fa-square-phone"></i>
-              <span>Đăng ký với sô điện thoại</span>
-            </div>
+              {phoneMode ? (
+                <input type="text" />
+              ) : (
+                <input type="email" placeholder="Nhập email của bạn" />
+              )}
+
+              <div className="register__clause">
+                <input type="checkbox" name="" id="register__clause--check" />
+                <label htmlFor="register__clause--check">
+                  Tôi đồng ý với{" "}
+                  <span className="register__clause--link">
+                    điều khoản & điều kiện
+                  </span>{" "}
+                  của ...
+                </label>
+              </div>
+              <button className="main-btn">Đăng ký</button>
+            </form>
+
+            <div className="register__boundary">Hoặc</div>
+
+            {!phoneMode ? (
+              <button
+                className="transparent-btn account__change--btn"
+                onClick={() => setPhoneMode(true)}
+              >
+                <i className=" fa-solid fa-square-phone account__change--phone"></i>
+                <span>Đăng ký với số điện thoại</span>
+              </button>
+            ) : (
+              <button
+                className="transparent-btn account__change--btn"
+                onClick={() => setPhoneMode(false)}
+              >
+                <i className="account__change--email fa-solid fa-envelope"></i>
+                <span>Đăng ký với Email</span>
+              </button>
+            )}
 
             <div className="register__login">
-              Bạn đã có sẵn tài khoản? Đăng nhập
+              Bạn đã có sẵn tài khoản?{" "}
+              <span className="register__login--link">&nbsp;Đăng nhập</span>
             </div>
           </div>
         </div>
