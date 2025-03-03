96d564ff02aa936ed74f8a04a11a88b0320f13c6
Kinji <linhqwertyuiop2gmail.com>
2025-03-03 15:24:36 +0700

    update 3_3_2025

--- src/app/ho-so/layout.tsx
+++ src/app/ho-so/layout.tsx
@@ -4,14 +4,14 @@ import CustomerNav from "@/components/customerNav/CustomerNav";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="auth container">
+    <div className="container">
       <div className="grid wide">
-        <div className="row">
-          <div className=" col l-3 m-3 s-0">
+        <div className="row no-gutters">
+          <div className="col l-3 m-3 s-0">
             <CustomerNav />
           </div>
-          <div className="col l-9 m-9 s-12">{children}</div>
-
+          <div className="col l-1"></div>
+          <div className="profile col l-8 m-8 s-12">{children}</div>
           <div className="l-0 m-0 s-12">
             <CustomerNav />
           </div>
--- src/app/ho-so/thong-tin/page.tsx
+++ src/app/ho-so/thong-tin/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Image from "next/image";
+import "../layout.css";
+import "./page.css";
 
 const ProfilePage = () => {
-  return <div>Thong Tin</div>;
+  return (
+    <>
+      <h3 className="profile__header">Thông tin của tôi</h3>
+      <div className=" main-container grid profile__info">
+        <div className="profile__info--top">
+          <Image
+            src="/assets/account/avatar_default.png"
+            alt="avatar default"
+            width={108}
+            height={108}
+          />
+        </div>
+        <div className="profile__info--top-input">
+          <i className="fa-solid fa-camera"></i>
+        </div>
+        <p>Nguyen Vu Linh</p>
+        <form className="profile__info--form" action="">
+          <div className="profile__info--form-block">
+            <label htmlFor="profile__info--form-name">Tên đầy đủ</label>
+            <input type="text" id="profile__info--form-name" />
+          </div>
+
+          <div className="profile__info--form-block">
+            <label htmlFor="">Giới tính</label>
+            <input type="radio" name="gender" id="" />
+            <span>Nam</span>
+            <input type="radio" name="gender" id="" />
+            <span>Nữ</span>
+            <input type="radio" name="gender" id="" />
+            <span>Khác</span>
+          </div>
+          <div className="profile__info--form-block">
+            <label htmlFor="profile__info--form-date">Ngày sinh</label>
+            <input type="date" name="" id="profile__info--form-date" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default ProfilePage;
