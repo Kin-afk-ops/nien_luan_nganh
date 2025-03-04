aaca39b3c18d7aa271234a6ff8a221334c9e01d4
Kinji <linhqwertyuiop2gmail.com>
2025-03-04 15:54:05 +0700

    update profile

--- src/app/ho-so/thong-tin/page.tsx
+++ src/app/ho-so/thong-tin/page.tsx
@@ -1,62 +1,97 @@
+"use client";
 import Image from "next/image";
 import "../layout.css";
 import "./page.css";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ProfilePage = () => {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
   return (
     <>
       <h3 className="profile__header">Thông tin của tôi</h3>
-      <div className=" main-container grid profile__info">
+      <div className="main-container profile__info">
         <div className="profile__info--top">
-          <Image
-            src="/assets/account/avatar_default.png"
-            alt="avatar default"
-            width={108}
-            height={108}
+          <label htmlFor="profile__info--avatar">
+            <Image
+              className="profile__info--top-image"
+              src="/assets/account/avatar_default.png"
+              alt="avatar default"
+              width={108}
+              height={108}
+            />
+            <div className="profile__info--top-input">
+              <i className="fa-solid fa-camera"></i>
+            </div>
+          </label>
+          <div className="profile__info--name">
+            <p>Nguyen Vu Linh</p>
+            <p>email</p>
+            <p>sdt</p>
+          </div>
+          <input
+            type="file"
+            className="display-none"
+            id="profile__info--avatar"
           />
         </div>
-        <div className="profile__info--top-input">
-          <i className="fa-solid fa-camera"></i>
-        </div>
-        <p>Nguyen Vu Linh</p>
+
         <form className="profile__info--form" action="">
           <div className="profile__info--form-block">
             <label htmlFor="profile__info--form-name">Tên đầy đủ</label>
-            <input type="text" id="profile__info--form-name" />
+
+            <input
+              className="profile__info--form-name success"
+              type="text"
+              id="profile__info--form-name"
+              placeholder="Nhập họ và tên"
+            />
           </div>
 
           <div className="profile__info--form-block">
             <label htmlFor="">Giới tính</label>
-            <input type="radio" name="gender" id="" />
-            <span>Nam</span>
-            <input type="radio" name="gender" id="" />
-            <span>Nữ</span>
-            <input type="radio" name="gender" id="" />
-            <span>Khác</span>
+            <div className="profile__info--form-gender">
+              <input type="radio" name="gender" id="" />
+              <span>Nam</span>
+              <input type="radio" name="gender" id="" />
+              <span>Nữ</span>
+              <input type="radio" name="gender" id="" />
+              <span>Khác</span>
+            </div>
           </div>
           <div className="profile__info--form-block">
             <label htmlFor="profile__info--form-date">Ngày sinh</label>
-            <input type="date" name="" id="profile__info--form-date" />
+
+            <DatePicker
+              className="profile__info--form-date"
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
           </div>
 
           <div className="profile__info--form-block">
-            <p>Địa chỉ</p>
-            <button>
+            <p className="profile__info--form-address">Địa chỉ</p>
+            <button className="profile__info--form-address-btn">
               <i className="fa-solid fa-plus"></i>
               Thêm mới
             </button>
           </div>
 
-          <div className="profile__info--form-block">
+          <div className="profile__info--form-block profile__info--form-introduce">
             <label htmlFor="profile__info--form-introduce">Giới thiệu</label>
             <textarea
-              placeholder="Nhập nhận xét của bạn về sản phẩm"
+              placeholder="Nhập giới thiệu"
               name=""
               id="profile__info--form-introduce"
-              cols="70"
+              cols="50"
               rows="10"
             />
           </div>
+
+          <button className="secondary-btn profile__info--form-save">
+            Lưu thay đổi
+          </button>
         </form>
       </div>
     </>
